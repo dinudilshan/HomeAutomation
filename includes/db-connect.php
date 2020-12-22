d17932e4509eced8b23b48c6e084bf71bdc0226d
Dinuka Ekanayake <dilshandinuka@gmail.com>
2020-12-22 16:28:09 +0530

    bd-config

--- includes/db-connect.php
+++ includes/db-connect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "127.0.0.1:3306";
-$username = "root";
-$password = "1234";
+$servername = "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "jgeuutxg2d8tkfoy";
+$password = "i0vkhagxtxbnvm83";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=home_auto", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=zze1i2jqrv0grugh", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
